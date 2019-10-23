543c1c41b7efb9dc6f372ec941b100adf7ee2e72
Steve McLaughlin <stevejrmc@gmail.com>
2019-10-23 15:28:52 -0700

    implemented functional stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,43 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  someInstance.storage = storage;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    // Get current length
+    var length = Object.keys(storage).length;
+    // Index first element at zero
+    if (length === 0) {
+      someInstance.storage[0] = value;
+      return length;
+    }
+    // Get Index/key for last value added
+    var lastIndex = Object.keys(storage)[length - 1];
+    // Set Index/key of new value to one more than last
+    someInstance.storage[lastIndex + 1] = value;
+    return length;
   };
 
+  // remove last element
   someInstance.pop = function() {
+    // If storage is empty, return/exit
+    if (!Object.keys(storage).length) {
+      return;
+    }
+    // Get current length
+    var length = Object.keys(storage).length;
+    // Get Index/key for last value added
+    var lastIndex = Object.keys(storage)[length - 1];
+    // Make copy of element to be removed
+    var removedValue = someInstance.storage[lastIndex].slice();
+    delete someInstance.storage[lastIndex];
+    return removedValue;
   };
 
   someInstance.size = function() {
+    // return size
+    return Object.keys(storage).length;
   };
 
   return someInstance;
