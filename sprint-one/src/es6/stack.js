fa98b65c375f81335e9c3d1afde472c4a3e04575
Steve McLaughlin <stevejrmc@gmail.com>
2019-10-25 16:33:02 -0700

    implemented stack es6 pattern

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,32 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  lastIndex() {
+    return Number(Object.keys(this.storage)[this.size() - 1]);
+  }
+
+  push(value) {
+    if (!this.size()) {
+      this.storage[0] = value;
+      return this.size();
+    }
+    this.storage[this.lastIndex() + 1] = value;
+    return this.size();
+  }
+
+  pop() {
+    if (!this.size()) {
+      return;
+    }
+    var removedValue = this.storage[Object.keys(this.storage)[this.lastIndex()]].slice();
+    delete this.storage[Object.keys(this.storage)[this.lastIndex()]];
+    return removedValue;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
