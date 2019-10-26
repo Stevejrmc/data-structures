b0a3c9b3a0cb5368ef96f8dd832722fd8dcba60e
Steve McLaughlin <stevejrmc@gmail.com>
2019-10-25 21:13:20 -0700

    pseudo coded implementation

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,57 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    // takes a value and adds it
+    // to the end of the list
+
+    // create a new node
+    var newNode = Node(value);
+
+    // if no currentTail (there is also no head)
+    //    set newNode to equal head & tail
+    //    return;
+
+    // set currentTail next to newNode
+    // set tail to newNode
+    // return;
   };
 
   list.removeHead = function() {
+    // removes the first node from the list
+    // and returns it's value
+
+    // get currentHead && set equal to removedHead
+    // set currentHead next equal to head
+    // return removedHead
   };
 
   list.contains = function(target) {
+    // returns a boolean reflecting whether
+    // or not a value is in the list
+
+    // if !list.head (nullHead === emptyList)
+    //    return false;
+
+    // if list.head.value === target
+    //    return true;
+
+    // if list.head.next === null (head is also tail so exit)
+    //    return false
+
+    // helper function:
+    // getNextNode() { currNode = currNode.next }
+
+    // recursively search:
+    // while currNode.value !== list.tail
+    //    if currNode.value === target
+    //        return true;
+    //    getNextNode();
+
+    // if list.tail.value === target
+    //    return true;
+
+    // target not in list
+    // return false;
   };
 
   return list;
