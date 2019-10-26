9cc0fd9f0c37a5c7ddfc464e431d7c70cc23eae6
Steve McLaughlin <stevejrmc@gmail.com>
2019-10-25 22:18:36 -0700

    implemented singly-linked-list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -13,10 +13,16 @@ var LinkedList = function() {
     // if no currentTail (there is also no head)
     //    set newNode to equal head & tail
     //    return;
+    if (!list.tail) {
+      list.head = newNode;
+      list.tail = newNode;
+      return;
+    }
 
     // set currentTail next to newNode
     // set tail to newNode
-    // return;
+    list.tail.next = newNode;
+    list.tail = newNode;
   };
 
   list.removeHead = function() {
@@ -24,37 +30,55 @@ var LinkedList = function() {
     // and returns it's value
 
     // get currentHead && set equal to removedHead
-    // set currentHead next equal to head
+    var removedHead = list.head.value;
+    // set head equal to currentHead next
+    list.head = list.head.next;
     // return removedHead
+    return removedHead;
   };
 
   list.contains = function(target) {
     // returns a boolean reflecting whether
     // or not a value is in the list
 
-    // if !list.head (nullHead === emptyList)
+    // if no head (nullHead === emptyList)
     //    return false;
+    if (!list.head) {
+      return false;
+    }
 
-    // if list.head.value === target
+    // if head or tail === target
     //    return true;
-
+    if (list.head.value === target || list.tail.value === target) {
+      return true;
+    }
     // if list.head.next === null (head is also tail so exit)
     //    return false
+    if (list.head.next === null) {
+      return false;
+    }
 
+    // Search middle of list:
+    var currNode = list.head.next;
     // helper function:
-    // getNextNode() { currNode = currNode.next }
+    var getNextNode = function() {
+      currNode = currNode.next;
+    };
 
     // recursively search:
     // while currNode.value !== list.tail
     //    if currNode.value === target
     //        return true;
     //    getNextNode();
-
-    // if list.tail.value === target
-    //    return true;
+    while (currNode !== list.tail) {
+      if (currNode.value === target) {
+        return true;
+      }
+      getNextNode();
+    }
 
     // target not in list
-    // return false;
+    return false;
   };
 
   return list;
