871e2c1630e3cc817296318e0c7e5f111f4c7696
Steve McLaughlin <stevejrmc@gmail.com>
2020-02-01 12:42:00 -0800

    Implement stack push and size method

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,19 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var instanceSize = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[instanceSize] = value;
+    instanceSize += 1;
   };
 
   someInstance.pop = function() {
   };
 
   someInstance.size = function() {
+    return instanceSize;
   };
 
   return someInstance;
