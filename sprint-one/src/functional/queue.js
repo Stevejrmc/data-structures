a9d07de45c52307d1e8c0e76ac83d6c2711c0352
Steve McLaughlin <stevejrmc@gmail.com>
2020-02-01 11:09:07 -0800

    Reset functional stack and queue exercise--clean slate

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,43 +1,15 @@
 var Queue = function() {
   var someInstance = {};
 
-  // Use an object with numeric keys to store values
   var storage = {};
 
-  // Implement the methods below
-
-  // Add to queue
   someInstance.enqueue = function(value) {
-    // Index first element at zero
-    if (someInstance.size() === 0) {
-      storage[0] = value;
-      return someInstance.size();
-    }
-    // Get Index/key for last value added
-    var lastIndex = Number(Object.keys(storage)[someInstance.size() - 1]);
-    // Set Index/key of new value to one more than last
-    storage[lastIndex + 1] = value;
-    return someInstance.size();
   };
 
-  // remove from queue
   someInstance.dequeue = function() {
-    // If storage is empty, return/exit
-    if (!someInstance.size()) {
-      return;
-    }
-    // Make a copy of value slated for deletion
-    var removedValue = storage[Object.keys(storage)[0]].slice();
-    // Delete value
-    delete storage[Object.keys(storage)[0]];
-    // Return what was deleted
-    return removedValue;
   };
 
-  // Get size
   someInstance.size = function() {
-    // return size
-    return Object.keys(storage).length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,35 +6,12 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
-    // Index first element at zero
-    if (someInstance.size() === 0) {
-      storage[0] = value;
-      return someInstance.size();
-    }
-    // Get Index/key for last value added
-    var lastIndex = Number(Object.keys(storage)[someInstance.size() - 1]);
-    // Set Index/key of new value to one more than last
-    storage[lastIndex + 1] = value;
-    return someInstance.size();
   };
 
-  // remove last element
   someInstance.pop = function() {
-    // If storage is empty, return/exit
-    if (!someInstance.size()) {
-      return;
-    }
-    // Get Index/key for last value added
-    var lastIndex = Object.keys(storage)[someInstance.size() - 1];
-    // Make copy of element to be removed
-    var removedValue = storage[lastIndex].slice();
-    delete storage[lastIndex];
-    return removedValue;
   };
 
   someInstance.size = function() {
-    // return size
-    return Object.keys(storage).length;
   };
 
   return someInstance;
