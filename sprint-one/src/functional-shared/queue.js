700c57f203ac3de14f6e99e532aabd2b3590580a
Steve McLaughlin <stevejrmc@gmail.com>
2020-02-01 13:04:29 -0800

    Reset functional-shared stack and queue exercise--clean slate

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,31 +1,6 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var newInstance = {};
-  newInstance.storage = {};
-  _.extend(newInstance, queueMethods);
-  return newInstance;
 };
 
-var queueMethods = {
-  enqueue: function(value) {
-    if (this.size() === 0) {
-      this.storage[0] = value;
-      return this.size();
-    }
-    var lastIndex = Object.keys(this.storage)[this.size() - 1];
-    this.storage[Number(lastIndex) + 1] = value;
-    return this.size();
-  },
-  dequeue: function() {
-    if (!this.size()) {
-      return;
-    }
-    var removedValue = this.storage[Object.keys(this.storage)[0]].slice();
-    delete this.storage[Object.keys(this.storage)[0]];
-    return removedValue;
-  },
-  size: function() {
-    return Object.keys(this.storage).length;
-  }
-};
+var queueMethods = {};
