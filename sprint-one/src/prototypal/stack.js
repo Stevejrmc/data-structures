51b7c47ca712f4c30f4e3c894f0f30e58ee9914e
Steve McLaughlin <stevejrmc@gmail.com>
2020-02-01 13:58:35 -0800

    Implement stack push and size methods

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,6 +1,19 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+
+  someInstance.storage = {};
+  someInstance.instanceSize = 0;
+
+  return someInstance;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value) {
+  this.storage[this.instanceSize] = value;
+  this.instanceSize += 1;
+};
+
+stackMethods.size = function() {
+  return this.instanceSize;
+};
