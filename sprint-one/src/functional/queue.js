e8a4265dd6c5df3a2ba5e43584946181921864c8
Steve McLaughlin <stevejrmc@gmail.com>
2020-02-01 12:14:08 -0800

    Implement functional stack

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -2,14 +2,27 @@ var Queue = function() {
   var someInstance = {};
 
   var storage = {};
+  var instanceSize = 0;
 
   someInstance.enqueue = function(value) {
+    storage[instanceSize] = value;
+    instanceSize += 1;
   };
 
   someInstance.dequeue = function() {
+    if (instanceSize > 0) {
+      instanceSize -= 1;
+      var removed = storage[0];
+      delete storage[0];
+      for (var i = 0; i < instanceSize; i++) {
+        storage[i] = storage[i + 1];
+      }
+      return removed;
+    }
   };
 
   someInstance.size = function() {
+    return instanceSize;
   };
 
   return someInstance;
