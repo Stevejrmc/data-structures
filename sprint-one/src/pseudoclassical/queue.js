7eafc7b457a02af4d3035aed679edad21c1074ed
Steve McLaughlin <stevejrmc@gmail.com>
2020-02-01 22:02:48 -0800

    Implement queue dequeue method

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -8,6 +8,20 @@ Queue.prototype.enqueue = function(value) {
   this.instanceSize += 1;
 };
 
+Queue.prototype.dequeue = function() {
+  if (this.instanceSize > 0) {
+    this.instanceSize -= 1;
+    var removed = this.storage[0];
+    if (this.instanceSize > 0) {
+      for (var i = 0; i < this.instanceSize; i++) {
+        this.storage[i] = this.storage[i + 1];
+      }
+    }
+
+    return removed;
+  }
+};
+
 Queue.prototype.size = function() {
   return this.instanceSize;
 };
