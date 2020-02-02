cec10ed8d3acacaa4448f5118cee1bb9ee1b7413
Steve McLaughlin <stevejrmc@gmail.com>
2020-02-01 22:57:43 -0800

    Implement es6 queue enqueue and size methods

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,5 +1,15 @@
 class Queue {
   constructor() {
+    this.storage = {};
+    this.instanceSize = 0;
   }
 
+  enqueue(value) {
+    this.storage[this.instanceSize] = value;
+    this.instanceSize += 1;
+  }
+
+  size() {
+    return this.instanceSize;
+  }
 }
