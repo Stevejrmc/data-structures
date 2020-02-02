a0476cb4cca2dc3ce3e4a6a1024193da2fe7d54d
Steve McLaughlin <stevejrmc@gmail.com>
2020-02-01 19:06:24 -0800

    Implement stack pop method

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -14,6 +14,15 @@ stackMethods.push = function(value) {
   this.instanceSize += 1;
 };
 
+stackMethods.pop = function() {
+  if (this.instanceSize > 0) {
+    this.instanceSize -= 1;
+    var removed = this.storage[this.instanceSize];
+    delete this.storage[this.instanceSize];
+    return removed;
+  }
+};
+
 stackMethods.size = function() {
   return this.instanceSize;
 };
