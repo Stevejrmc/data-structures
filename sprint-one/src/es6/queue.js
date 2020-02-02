435643ab2737f2d60befbc82df186160ec66a498
Steve McLaughlin <stevejrmc@gmail.com>
2020-02-01 23:00:43 -0800

    Implement es6 queue dequeue method

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -9,6 +9,21 @@ class Queue {
     this.instanceSize += 1;
   }
 
+  dequeue() {
+    if (this.instanceSize > 0) {
+      this.instanceSize -= 1;
+      const removed = this.storage[0];
+      delete this.storage[0];
+      if (this.instanceSize > 0) {
+        for (var i = 0; i < this.instanceSize; i++) {
+          this.storage[i] = this.storage[i + 1];
+        }
+      }
+
+      return removed;
+    }
+  }
+
   size() {
     return this.instanceSize;
   }
