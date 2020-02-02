ee446afbd527a4c1b783e654e776a54c48e2e6cb
Steve McLaughlin <stevejrmc@gmail.com>
2020-02-01 21:42:54 -0800

    Implement stack push and size methods

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,2 +1,13 @@
 var Stack = function() {
+  this.storage = {};
+  this.instanceSize = 0;
+};
+
+Stack.prototype.push = function(value) {
+  this.storage[this.instanceSize] = value;
+  this.instanceSize += 1;
+};
+
+Stack.prototype.size = function() {
+  return this.instanceSize;
 };
