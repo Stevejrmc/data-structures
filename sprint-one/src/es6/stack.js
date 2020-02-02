e2aa0a4c358a7cf521234e1f8a1839db2b4c5d28
Steve McLaughlin <stevejrmc@gmail.com>
2020-02-01 22:25:25 -0800

    Implement es6 stack push and size methods

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,5 +1,16 @@
 class Stack {
   constructor() {
+    this.storage = {};
+    this.instanceSize = 0;
+  }
+
+  push(value) {
+    this.storage[this.instanceSize] = value;
+    this.instanceSize += 1;
+  }
+
+  size() {
+    return this.instanceSize;
   }
 
 }
