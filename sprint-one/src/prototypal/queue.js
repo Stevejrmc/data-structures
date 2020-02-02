fd62bccb0fa917e53a1abafdcb5a19a8da67d2fe
Steve McLaughlin <stevejrmc@gmail.com>
2020-02-01 19:21:21 -0800

    Implement queue enqueue and size methods

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,6 +1,17 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  someInstance.instanceSize = 0;
+
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[this.instanceSize] = value;
+    this.instanceSize += 1;
+  },
+  size: function() {
+    return this.instanceSize;
+  }
+};
