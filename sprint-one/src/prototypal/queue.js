34f1a0d8a75ae13e6ed6929f78951d2d03307666
Steve McLaughlin <stevejrmc@gmail.com>
2020-02-01 21:16:03 -0800

    Implement queue dequeue method

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -11,6 +11,18 @@ var queueMethods = {
     this.storage[this.instanceSize] = value;
     this.instanceSize += 1;
   },
+  dequeue: function() {
+    if (this.instanceSize > 0) {
+      this.instanceSize -= 1;
+      var removed = this.storage[0];
+      if (this.instanceSize > 0) {
+        for (var i = 0; i < this.instanceSize; i++) {
+          this.storage[i] = this.storage[i + 1];
+        }
+      }
+      return removed;
+    }
+  },
   size: function() {
     return this.instanceSize;
   }
