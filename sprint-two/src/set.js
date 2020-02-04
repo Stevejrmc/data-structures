dd6579d2c3b5cd986960c1ed6ec8bbf2a657fcf2
Steve McLaughlin <stevejrmc@gmail.com>
2020-02-03 21:46:25 -0800

    Reset set exercise--clean slate

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,24 +1,20 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = {};
+  set._storage = null; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
-  return this._storage[item] === item ? true : false;
 };
 
 setPrototype.remove = function(item) {
-  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
- * The functions/methods complexity are constant.
  */
