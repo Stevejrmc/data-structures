dcbc423c0389b8127d3645cb372765d702747ccb
Steve McLaughlin <stevejrmc@gmail.com>
2020-02-03 21:59:31 -0800

    Reset hash-table exercise--clean slate

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,43 +7,14 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  if (!this._storage.get(index)) {
-    var bucket = [];
-    bucket.push([k, v]);
-    this._storage.set(index, bucket);
-  }
-  var bucket = this._storage.get(index);
-  if (bucket[0][0] === k) {
-    bucket[0][1] = v;
-  } else {
-    bucket[1] = [k, v];
-  }
-  this._storage.set(index, bucket);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var bucket = this._storage.get(index);
-  if (!bucket) {
-    return undefined;
-  }
-  return bucket[0][0] === k ? bucket[0][1] : bucket[1][1];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var bucket = this._storage.get(index);
-  if (bucket[0][0] === k) {
-    if (bucket.length < 2) {
-      this._storage.set(index, undefined);
-      return;
-    }
-    bucket.shift();
-    this._storage.set(index, bucket);
-  } else {
-    bucket.pop();
-    this._storage.set(index, bucket);
-  }
 };
 
 
