55b0e9fb36fd43ab6cf352a7b94b2ac84be5c4de
Steve McLaughlin <stevejrmc@gmail.com>
2020-02-10 20:10:11 -0800

    Implement BST insert method

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,29 @@
 var BinarySearchTree = function(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
 };
 
+BinarySearchTree.prototype.insert = function(value) {
+  if (this.value > value) {
+    if (this.left) {
+      this.left.insert(value);
+    } else {
+      this.left = new BinarySearchTree(value);
+    }
+  } else {
+    if (this.right) {
+      this.right.insert(value);
+    } else {
+      this.right = new BinarySearchTree(value);
+    }
+  }
+};
+
+BinarySearchTree.prototype.contains = function(value) {
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(cb) {};
 
 /*
  * Complexity: What is the time complexity of the above functions?
